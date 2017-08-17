918959c906b27a0d1322450e14cf040c01c9ab84
hugodieb <hugodieb.hd@gmail.com>
2017-08-17 13:05:33 -0300

    Comunicação entre templates vuejs

---
+++ src/components/User.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button type="button" class="btn btn-primary" @click="changeName">Change Name...</button>
+        <span>{{name}}</span> /
+        <span>{{age}}</span>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :myName="name"
+                                  @nameWasReset="name = $event"
+                                  @ageWasReset="age = $event"
+                                  :resetFn="resetName"
+                                  :userAge="age">
+                                  </app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit :userAge="age"
+                                @ageWasReset="age = $event"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data() {
+          return {
+            name: "Hugo",
+            age: 27,
+          };
+        },
+        methods: {
+          changeName() {
+            this.name = "Mara"
+          },
+          resetName() {
+            this.name = "Hugo"
+          },
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User name: {{ switchName() }} </p>
+        <p>User age: {{ userAge }} </p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetFn">Reset Name Fn</button>
+        <button @click="resetAge">Reset Age</button>
+    </div>
+</template>
+
+<script>
+  export default {
+    /*props: ['myName'],*/
+    props: {
+      myName: {
+        type: String,
+      },
+      resetFn: Function,
+      userAge: Number,
+    },
+    methods: {
+      switchName() {
+        return this.myName.split("").reverse().join("");
+      },
+      resetName() {
+        this.myName = "Sheik";
+        this.$emit('nameWasReset', this.myName);
+      },
+      resetAge() {
+        this.userAge = 45;
+        this.$emit('ageWasReset', this.userAge);
+      },
+    },
+  }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
